9c3bee4aec67ca4e40dd611aa79bc43414208ecb
sanisamoj <marioartec39@gmail.com>
2025-05-18 23:30:00 -0300

    force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,12 @@ import HomePage from "./components/HomePage"
 import { GlobalRepository } from "./repository"
 import { Version } from "./Version"
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const repository = GlobalRepository.getInstance()
   const actualVersion: Version = await repository.getLatestVersion()
   const allVersions: Version[] = await repository.getAllVersions()
-  console.log(actualVersion, allVersions)
 
   return (
     <HomePage actualVersion={actualVersion} allVerssions={allVersions}/>
